6e6c3712bf8ce6d3067af594d0557740a8307da5
keerthivasankrishnamurthy <kkrish39@asu.edu>
2020-11-19 02:03:23 -0500

    Added dummy methods and integrated the flow

--- app/src/main/java/com/example/asuforia_library/Asuforia.java
+++ app/src/main/java/com/example/asuforia_library/Asuforia.java
@@ -9,9 +9,31 @@ public class Asuforia {
     Image image;
     Surface surface;
 
+    static {
+        System.loadLibrary("opencv_java");
+    }
+
     Asuforia(PoseListener poseListener, Image im, Surface sf){
         this.poseListener = poseListener;
         this.image = im;
         this.surface = sf;
+    }
+
+    public Asuforia() {
+
+    }
+
+    void onImageAvailable(){
+        //Call Native pose Estimation
+        this.poseListener.onPose();
+    }
+
+    void startEstimation(){
+        // Start camera
+        onImageAvailable();
+    }
+
+    void endEstimation(){
+        // Stop camera
     }
 }
--- app/src/main/java/com/example/asuforia_library/MainActivity.java
+++ app/src/main/java/com/example/asuforia_library/MainActivity.java
@@ -2,18 +2,50 @@ package com.example.asuforia_library;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Surface;
 
-public class MainActivity extends AppCompatActivity implements  PoseListener{
+public class MainActivity extends AppCompatActivity{
+    Asuforia af;
+    Image im;
+    Surface sf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PoseListener poseListener = new PoseListener() {
+            @Override
+            public void onPose() {
+                System.out.println("Estimated Pose. About to color");
+            }
+        };
+
+        af = new Asuforia(poseListener, im, sf);
     }
 
     @Override
-    public void onPose(    ) {
-            // color my surface
+    protected void onStart() {
+        System.out.println("Starting Pose Estimation");
+
+        /* On entering foreground, start Estimation */
+        af.startEstimation();
+
+        super.onStart();
+//        getDelegate().onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        System.out.println("Ending Pose Estimation");
+
+        /* On leaving foreground, End estimation */
+        af.endEstimation();
+
+        super.onStop();
+//        getDelegate().onStop();
     }
 }
